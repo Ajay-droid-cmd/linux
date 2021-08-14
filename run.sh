8c9420cc9841e7b263af25956e94b2f69984d5df
Ajay-droid-cmd <ajay.t5102001@gmail.com>
2021-08-14 22:27:12 +0530

    added clean

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Start";
-ls -l
 sudo apt update 
 echo -e $TEXT_BLUE
 echo 'APT update finished...'
@@ -9,5 +8,6 @@ sudo apt-get upgrade
 echo -e $TEXT_GREEN
 echo 'APT upgrade finished...'
 echo -e $TEXT_RESET
-
+sudo apt autoremove
+echo 'Cleaning update'
 echo "end";
